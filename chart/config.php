03dc25489e4b87ee3ae2a29639aa73ca03648d37
tohadibrex <tohadibro@gmail.com>
2020-06-16 15:32:37 +0700

    Add files via upload

---
+++ chart/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'pmb';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$conn = new mysqli($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+if($conn-> connect_errno){
+	echo die("Gagal Menghubungkan ke Databases".$conn->connect-error);
+}
+?>
